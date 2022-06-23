0ed9ec043e5ba5511f9104a9194102afb47a3fc8
robin <rsantanall@cdsi.com.pe>
2022-06-23 16:06:44 -0500

    Nueva URL para usar el REST

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL_BASE : 'https://ab97-2001-1388-91-1169-69a1-99c0-6e00-63bd.ngrok.io/spring-boot-backend-pvCdsi/api'
+  URL_BASE : 'https://64d3-2001-1388-90-65db-69a1-99c0-6e00-63bd.ngrok.io/spring-boot-backend-pvCdsi/api'
 };
